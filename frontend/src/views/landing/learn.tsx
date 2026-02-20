03d57dd41ccbb7093277103dd5f83146e130f971
Noor <159580993+light03s@users.noreply.github.com>
2026-02-19 18:08:35 -0600

    This is the second landing page, aka the learn page
    This is being branched off of development. There is still an issue with the header due to conflicting styling. The css page is located in the learn folder and all components have been placed in the components folder.

--- frontend/src/views/landing/learn.tsx
+++ frontend/src/views/landing/learn.tsx
@@ -1,5 +1,22 @@
-const Learn = () => {
-  return <div>Learn View</div>;
-};
+import "./Learn.css"
+import Navbar from "@/components/Navbar"
+import LeftBar from "@/components/LeftBar"
+import SearchBar from "@/components/SearchBar"
+import Main from "@/components/Main"
+import LearnButton from "@/components/LearnButton"
+import LearnFooter from "@/components/LearnFooter"
 
-export default Learn;
+function Learn() {
+  return (
+    <div>
+      <Navbar />
+      <LeftBar />
+      <SearchBar />
+      <Main />
+      <LearnButton />
+      <LearnFooter />
+    </div>
+  )
+}
+
+export default Learn
