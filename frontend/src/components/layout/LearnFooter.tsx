38d7e67bc2edb4628c2cb1b321d6bb2e4c568d14
Noor <159580993+light03s@users.noreply.github.com>
2026-03-04 17:22:20 -0600

    Corrected the logo imports in the header and footer

--- frontend/src/components/layout/LearnFooter.tsx
+++ frontend/src/components/layout/LearnFooter.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/logo.svg";
+import logo from "@/assets/logoicon.svg";
 
 function LearnFooter() {
     return (
--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/logo.svg"
+import logo from "@/assets/logoicon.svg"
 import { Button } from "@/components/ui/button"
 
 function Navbar() {
