679f60b9939b2d0590363d6eda0265c81767ceb4
Noor <159580993+light03s@users.noreply.github.com>
2026-03-03 20:58:03 -0600

    Removed header and footer imports

--- frontend/src/views/landing/learn.tsx
+++ frontend/src/views/landing/learn.tsx
@@ -1,20 +1,16 @@
 import "@/Learn.css"
-import Navbar from "@/components/layout/Navbar"
 import LeftBar from "@/components/learn/LeftBar"
 import SearchBar from "@/components/learn/SearchBar"
 import Main from "@/components/learn/Main"
 import LearnButton from "@/components/learn/LearnButton"
-import LearnFooter from "@/components/learn/LearnFooter"
 
 function Learn() {
   return (
     <div>
-      {/* <Navbar /> */}
       <LeftBar />
       <SearchBar />
       <Main />
       <LearnButton />
-      {/* <LearnFooter /> */}
     </div>
   )
 }
