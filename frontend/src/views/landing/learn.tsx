183ddff94cfd9f66807da90545e976413d20ad51
Noor <159580993+light03s@users.noreply.github.com>
2026-03-11 15:50:03 -0600

    Update learn.tsx

--- frontend/src/views/landing/learn.tsx
+++ frontend/src/views/landing/learn.tsx
@@ -8,11 +8,6 @@ function Learn() {
     <div className="min-h-screen flex">
       <LeftBar />
       <div className="flex-1 md:ml-[280px] lg:ml-[320px]">
-
-        {/* <div className="pt-4 md:pt-6">
-          <SearchBar />
-        </div> */}
-        {/* where the cards are imported */}
         <div>
           <Main />
         </div>
