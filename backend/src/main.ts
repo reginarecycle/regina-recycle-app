8fe8bf229ae01a08509aa1251ccc1933d3caddb0
Temidayo George <george.temidayo@gmail.com>
2026-03-14 10:24:37 -0600

    fix: changed port to 3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -33,9 +33,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT!);
 }
-
-bootstrap();
 
 export default bootstrap;
