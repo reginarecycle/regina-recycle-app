02ede54d4e9623368871941541853bf6754f2fcc
Temidayo George <george.temidayo@gmail.com>
2026-03-14 16:19:48 -0600

    fix: render swagger ui

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,61 +1,17 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-// import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-// import { ResponseInterceptor } from './common/interceptor/response.interceptor';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.enableCors({
-//     origin: '*',
-//   });
-
-//   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-//   app.useGlobalInterceptors(new ResponseInterceptor());
-//   app.useGlobalFilters(new HttpExceptionFilter());
-
-//   const config = new DocumentBuilder()
-//     .setTitle('ReginaRecycle API')
-//     .setDescription('ReginaRecycle backend API documentation')
-//     .setVersion('1.0')
-//     .addBearerAuth(
-//       {
-//         type: 'http',
-//         scheme: 'bearer',
-//         bearerFormat: 'JWT',
-//       },
-//       'JWT-auth',
-//     )
-//     .build();
-
-//   const document = SwaggerModule.createDocument(app, config);
-//   SwaggerModule.setup('docs', app, document);
-
-//   await app.listen(process.env.PORT!);
-// }
-
-// bootstrap();
-// export default bootstrap;
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Global pipes, filters, interceptors first
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  // ✅ Swagger setup
   const config = new DocumentBuilder()
     .setTitle('ReginaRecycle API')
     .setDescription('ReginaRecycle backend API documentation')
@@ -71,10 +27,12 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
-  // ✅ Export swagger.json inside bootstrap so document is accessible
-  // fs.writeFileSync('./swagger.json', JSON.stringify(document, null, 2));
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'ReginaRecycle API Docs',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
