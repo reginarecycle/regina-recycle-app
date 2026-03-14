04827088313b38dde99c028b15b082f2f128463c
Noor <159580993+light03s@users.noreply.github.com>
2026-03-14 03:25:28 -0600

    Update index.tsx

--- frontend/src/views/customer/home/index.tsx
+++ frontend/src/views/customer/home/index.tsx
@@ -1,11 +1,15 @@
 import WelcomeMessage from "@/components/customer-dashboard/welcome-message";
 
 const UserHome = () => {
+<<<<<<< Updated upstream
   return (
     <div>
       <WelcomeMessage />
     </div>
   )
+=======
+  return <div>UserHome dskfjhsdjfhsdkjfjh</div>;
+>>>>>>> Stashed changes
 };
 
 export default UserHome;
