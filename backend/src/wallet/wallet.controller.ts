5d740e52c4e4d581100b41e4d06221f52b52c77b
Noor <159580993+light03s@users.noreply.github.com>
2026-03-18 00:48:02 -0600

    Added Pagination Import

--- backend/src/wallet/wallet.controller.ts
+++ backend/src/wallet/wallet.controller.ts
@@ -6,27 +6,20 @@ import {
   Body,
   Param,
   Query,
-  ParseIntPipe,
-  DefaultValuePipe,
 } from '@nestjs/common';
-import { WithdrawDto } from 'src/common/dto/withdraw.dto';
-import { WalletTransactionDTO } from './dto/wallet-transaction.dto';
-import { CustomerWalletDto } from './dto/customerWallet.dto';
-import { CollectorWalletDto } from './dto/collectorWallet.dto';
 import { Auth } from 'src/common/decorator/auth.decorator';
 import { PaginationDto } from 'src/common/pagination/pagination.dto';
 import { CurrentUser } from '../auth/decorator/current-user.decorator';
 import { CustomerWithdrawFundsDto } from './dto/customerWithdrawFunds.dto';
 import { TopUpRequestsDto } from './dto/topUpRequests.dto';
 
-
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) { }
 
-  //  create a wallet summary for a customer
-  // wallets are not created since we can only have 1 wallet per user
-  @Post('customer')
+  // create a wallet summary for a customer
+  // wallets are not created manually since we can only have 1 wallet per user
+  @Get('customer')
   @Auth()
   async getCustomerWallet(@CurrentUser('id') userId: string) {
     return this.walletService.getOrCreateCustomerWallet(userId);
@@ -38,6 +31,7 @@ export class WalletController {
   async getCollectorWallet(@CurrentUser('id') userId: string) {
     return this.walletService.getOrCreateCollectorWallet(userId);
   }
+
   // get the balance for the wallet
   @Get('balance')
   @Auth()
@@ -50,12 +44,19 @@ export class WalletController {
   @Auth()
   async getWalletTransactions(
     @CurrentUser('id') userId: string,
-    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
-    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query() paginationDto: PaginationDto,
     @Query('search') search?: string,
   ) {
-    return this.walletService.getWalletTransactions(userId, page, limit, search);
+    const { page = 1, limit = 10 } = paginationDto;
+
+    return this.walletService.getWalletTransactions(
+      userId,
+      page,
+      limit,
+      search,
+    );
   }
+
   // get a single wallet transaction by id
   @Get('transactions/:transactionId')
   @Auth()
@@ -96,7 +97,6 @@ export class WalletController {
   ) {
     return this.walletService.getWalletStats(userId, startDate, endDate);
   }
-
 
   // wallet earnings
   @Get('earnings')
