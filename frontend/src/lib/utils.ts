7da547bcb7f5a3213e304edb59676ae700f77152
Temidayo George <george.temidayo@gmail.com>
2026-03-19 12:42:18 -0600

    feat: api integration for auth

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,11 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type { Notification, NotificationType } from "@/types/notification";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-import type { Notification, NotificationType } from "@/types/notification";
 
 export const getTypeStyles = (type: NotificationType) => {
   const styles = {
