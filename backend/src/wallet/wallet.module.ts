8baf027c315d3666e6ae2adc652b0dc1bcacea03
Noor <159580993+light03s@users.noreply.github.com>
2026-03-19 00:55:22 -0600

    Removed extra notification and fixed erorrs

--- backend/src/wallet/wallet.module.ts
+++ backend/src/wallet/wallet.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [PrismaModule, NotificationsModule],
   controllers: [WalletController],
   providers: [WalletService],
 })
