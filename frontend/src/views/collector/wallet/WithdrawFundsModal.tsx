3e9510815e9fb8ceab87e5ef67ae97cecbce1697
Marjan421 <marjanripa421@gmail.com>
2026-03-21 05:11:57 -0600

    fix: withdraw modal build issue

--- frontend/src/views/collector/wallet/WithdrawFundsModal.tsx
+++ frontend/src/views/collector/wallet/WithdrawFundsModal.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Building2 } from "lucide-react";
-import { ModalShell, ModalFooter, StepIndicator, AmountInput, QuickAmountBtn, SuccessScreen, ModalInput } from "./shared";
+import { ModalShell, ModalFooter, StepIndicator, AmountInput, QuickAmountBtn, SuccessScreen } from "./shared";
 import { QUICK_AMOUNTS } from "./constants";
 
 interface WithdrawFundsModalProps {
