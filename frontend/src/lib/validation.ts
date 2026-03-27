d2ef38ea3fa8b064580ee50db140ba711ec342d0
Sumayya <nurysumayya@gmail.com>
2026-03-26 22:18:13 -0600

    Customer profile api consumption finalfix (#97)
    * added update password message & loading effect in update prefrence
    
    * update email disabled, loading in update prefrence button , password validation check
    
    * added toast notification

--- frontend/src/lib/validation.ts
+++ frontend/src/lib/validation.ts
@@ -108,8 +108,16 @@ export type ProfileDetailsFormValues = z.infer<typeof profileDetailsSchema>;
 export const changePasswordSchema = z
   .object({
     currentPassword: z.string().min(8, "Password must be at least 8 characters"),
-    newPassword: z.string().min(8, "Password must be at least 8 characters"),
-    confirmPassword: z.string().min(6, "Confirm Password must be at least 6 characters"),
+
+    newPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character"),
+
+    confirmPassword: z.string().min(8, "Confirm Password must be at least 8 characters"),
   })
   .refine((data) => data.newPassword === data.confirmPassword, {
     message: "Passwords do not match",
